2389254463a4a005439bab848e858da54b9fb598
hccazado <heitorcazado@gmail.com>
2025-03-12 11:12:22 -0300

    first commit

---
+++ week1/console/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic;
+
+int year = DateTime.Now.Year;
+var christmasDate = new DateTime(year, 12, 25);
+var today = DateTime.Now;
+double daysUntilChristmas = christmasDate.Subtract(DateTime.Today).TotalDays;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is "+today);
+Console.WriteLine($"There are {daysUntilChristmas} days until next Christmas");
